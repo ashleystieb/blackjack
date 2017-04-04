76f8f6fac833063d031c030eb6e7b7913f50bd25
ashleystieb <ashleystieb@gmail.com>
2017-04-03 21:08:17 -0700

    added Deck class

--- blackjack.py
+++ blackjack.py
@@ -7,3 +7,20 @@ class Card:
 
     def __str__(self):
         return self.rank + " of " + self.suit
+
+class Deck:
+    def __init__(self):
+
+        self.suit = ['Heart','Spade','Diamond','Club']
+        self.rank = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace']
+        self.deck = []
+
+    def generate(self):
+
+        for i in self.suit:
+            for p in self.rank:
+                self.deck.append(Card(i,p))
+
+    def print_deck(self):
+        for i in self.deck:
+            print(i)
